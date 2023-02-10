8ab4742e7ea43dbad1f2127a842461953f74435c
Roman Yadushinskiy <105167501+RomanYadushinskiy@users.noreply.github.com>
2023-02-10 10:52:09 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+export const App = () => {
+  return (
+    <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 20,
+          color: '#010101',
+        }}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </div>
+  );
+};
+
+
+
+
