5531477f5bea3713dc23d648b48448ef4e4a86e7
rdush <rdush824@gmail.com>
2023-02-12 23:55:52 +0200

    hw done

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -21,11 +21,17 @@ export const LoginForm = () => {
     <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
       <label className={css.label}>
         Email
-        <input type="email" name="email" />
+        <input
+          type="email"
+          name="email"
+          placeholder="user@mail.com"/>
       </label>
       <label className={css.label}>
         Password
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"/>
       </label>
       <button type="submit">Log In</button>
     </form>
--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -22,15 +22,24 @@ export const RegisterForm = () => {
     <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
       <label className={css.label}>
         Username
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          placeholder='Username'/>
       </label>
       <label className={css.label}>
         Email
-        <input type="email" name="email" />
+        <input
+          type="email"
+          name="email"
+          placeholder='user@mail.com'/>
       </label>
       <label className={css.label}>
         Password
-        <input type="password" name="password" />
+        <input
+          type="password"
+          name="password"
+          placeholder='Password'/>
       </label>
       <button type="submit">Register</button>
     </form>
